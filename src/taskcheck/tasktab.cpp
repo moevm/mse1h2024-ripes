6636141448ee7451a44f79b22b615575cfff9b86
Манцева Татьяна <55297427+mantsevat@users.noreply.github.com>
2024-04-25 01:15:16 +0300

    change tasktab with taskchecker

--- src/taskcheck/tasktab.cpp
+++ src/taskcheck/tasktab.cpp
@@ -14,71 +14,50 @@ TaskTab::TaskTab(QToolBar *toolbar, EditTab *edittab, QWidget *parent)
 	m_ui->setupUi(this);
 	this->edittab = edittab;
 	
-	connect(m_ui->sectionBox, &QComboBox::currentIndexChanged,
-	this, &TaskTab::changeSection);
-	connect(m_ui->numberBox, &QComboBox::currentIndexChanged,
-	this, &TaskTab::changeTask);
+	connect(m_ui->taskBox, &QComboBox::currentIndexChanged,
+	this, &TaskTab::showTask);
 	connect(m_ui->checkButton, &QPushButton::clicked, this, &TaskTab::checkTask);
 	
-	m_ui->sectionBox->setEditable(true);
-	m_ui->sectionBox->setInsertPolicy(QComboBox::NoInsert);
-	m_ui->sectionBox->setPlaceholderText(QStringLiteral("Выберите раздел"));
-	m_ui->sectionBox->setCurrentIndex(-1);
-	m_ui->numberBox->setEditable(false);
-	m_ui->numberBox->setInsertPolicy(QComboBox::NoInsert);
-	m_ui->numberBox->setPlaceholderText(QStringLiteral("Выберите задание"));
-	m_ui->numberBox->setCurrentIndex(-1);
+	m_ui->taskBox->setEditable(true);
+	m_ui->taskBox->setInsertPolicy(QComboBox::NoInsert);
+	m_ui->taskBox->setPlaceholderText(QStringLiteral("Выберите задание"));
+	m_ui->taskBox->setCurrentIndex(-1);
+
 	m_ui->taskText->setReadOnly(true);
 	m_ui->answerText->setReadOnly(true);
-	m_ui->checkButton->setEnabled(false);	
+	m_ui->checkButton->setEnabled(false);
+	
+	std::vector<Task> tasks = taskchecker.getTasks();
 
-	for (unsigned int i = 0; i < taskchecker.getSectionNum(); i++){
-		m_ui->sectionBox->addItem(QString::number(i+1), 0);
+	for (unsigned int i = 0; i < tasks.size(); i++){
+		m_ui->taskBox->addItem(QString::fromStdString(std::to_string(tasks[i].id) + ": " + tasks[i].title), tasks[i].id);
 	}
 
 }
 
 TaskTab::~TaskTab() { delete m_ui; }
 
-void TaskTab::changeSection(){
-	if(m_ui->sectionBox->currentIndex() > -1){
-		currentSection = std::stoi(m_ui->sectionBox->currentText().toStdString());
-	
-		m_ui->numberBox->setEditable(true);
-		m_ui->numberBox->clear();
-		m_ui->numberBox->setCurrentIndex(-1);
-
-		for(const unsigned int& num : taskchecker.getSectionTasks(currentSection)){
-			m_ui->numberBox->addItem(QString::number(num), num);
-		}
-	}
-}
-
-void TaskTab::checkTask(){
+void TaskTab::checkTask()
+{
 	std::string answer;
 	auto res = ProcessorHandler::getAssembler()->assembleRaw(
       edittab->getAssemblyText(), &IOManager::get().assemblerSymbols());
   	
   	if (res.errors.size() == 0) {
-		answer = taskchecker.checkTask(edittab->getAssemblyText(), currentNumber, currentSection);
+		answer = taskchecker.checkTask(edittab->getAssemblyText(), currentTaskId);
 	} else {
 		answer = "No check for program with syntax errors";
 	}
-	m_ui->answerText->setPlainText(QString::fromStdString(answer) + " - answer\n" + 
-	"Current program is:\n" + edittab->getAssemblyText() );
+	m_ui->answerText->setPlainText(QString::fromStdString(answer));
 }
 
-void TaskTab::changeTask(){
-	if(m_ui->numberBox->currentIndex() > -1){
-		currentNumber = std::stoi(m_ui->numberBox->currentText().toStdString());
-
+void TaskTab::changeTask()
+{
+	if(m_ui->taskBox->currentIndex() > -1){
+		currentTaskId = m_ui->taskBox->itemData(m_ui->taskBox->currentIndex()).toUInt();
+		std::string txt = taskchecker.findTask(currentTaskId);
+		m_ui->taskText->setPlainText(QString::fromStdString(txt);
 		m_ui->checkButton->setEnabled(true);
-		Task *current = taskchecker.findTask(currentSection, currentNumber);
-		if (current != nullptr) {
-			m_ui->taskText->setPlainText(QString::fromStdString(current->getText()));
-		} else {
-			m_ui->taskText->setPlainText("No task");
-		}
 	} else {
 		m_ui->checkButton->setEnabled(false);
 	}	
