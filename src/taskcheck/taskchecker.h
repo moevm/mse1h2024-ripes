62441b76deb7da0e1afb5ee9daedc59abf3aacc7
Манцева Татьяна <55297427+mantsevat@users.noreply.github.com>
2024-04-25 01:14:12 +0300

    Update taskchecker.h

--- src/taskcheck/taskchecker.h
+++ src/taskcheck/taskchecker.h
@@ -1,21 +1,22 @@
 #include <QString>
-#include "task.h"
+
+class Task{
+
+public:
+	Task(std::string title, std::string text, unsigned int id);
+	std::string title;
+	std::string text;
+	unsigned int id;
+}
+
 class TaskChecker {
 
 public:
 	TaskChecker();
 	~TaskChecker();
-	std::string checkTask(QString program, unsigned int section, unsigned int number);
-	unsigned int getSectionNum() const;
-	std::vector<unsigned int> getSectionTasks(unsigned int section) const;
-	Task* findTask(unsigned int section, unsigned int number);
+	std::string checkTask(QString program, unsigned int t_id);
+	std::vector<Task> getTasks() const;
+	std::string findTask(unsigned int t_id);
 private:
 	std::vector<Task> tasks;
-	std::vector<std::string> sections;
-
-	void setTasks();
-	void setSections();
-	bool checkPrintVal(QString program, std::string input, std::string output, std::string &answer);
-	bool checkReturnVal(QString program, std::string input, std::string output, std::string &answer);	
-
 };
