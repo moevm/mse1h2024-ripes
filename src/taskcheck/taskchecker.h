d292ebc1af973176a8e4824e78a569e011ae16e4
mantsevat <mantsevat@gmail.com>
2024-04-28 17:02:31 +0300

    fix cpp to js code

--- src/taskcheck/taskchecker.h
+++ src/taskcheck/taskchecker.h
@@ -7,7 +7,7 @@ public:
 	std::string title;
 	std::string text;
 	unsigned int id;
-}
+};
 
 class TaskChecker {
 
@@ -15,7 +15,7 @@ public:
 	TaskChecker();
 	~TaskChecker();
 	std::string checkTask(QString program, unsigned int t_id);
-	std::vector<Task> getTasks() const;
+	std::vector<Task> getTasks();
 	std::string findTask(unsigned int t_id);
 private:
 	std::vector<Task> tasks;
