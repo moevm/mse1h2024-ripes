ea8412d31ef5a0cac42c25a0a0553c7749c4f53c
mantsevat <mantsevat@gmail.com>
2024-04-28 17:14:53 +0300

    fix tasks check

--- lti_server/server/infra/tasks/task_3.py
+++ lti_server/server/infra/tasks/task_3.py
@@ -1,9 +1,9 @@
 import subprocess
+import os
 from .ripes_parser import parse_ripes
 
 def check(student_id):
-	run1 = parse_ripes(subprocess.check_output("../Ripes --mode cli --src ../tmp_solutions/{sid}_1.s -t asm --proc RV64_SS --regs --json -v"
-	.format(sid = student_id).split()).decode("utf-8"))
+	run = parse_ripes(subprocess.check_output("{d}/Ripes --mode cli --src ./tmp_solution/{sid}_3.s -t asm --proc RV64_SS --regs --json -v".format(d=os.path.dirname(__file__), sid = student_id).split()).decode("utf-8"))	
 	return "All tests passed!"
 	
 def get_txt():
