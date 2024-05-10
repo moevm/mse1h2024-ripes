e5a4a7565ab3ae9c4497befea87a39b71404d330
mantsevat <mantsevat@gmail.com>
2024-05-10 10:01:52 +0300

    Fix docker and add separate install file

--- lti_server/server/infra/tasks/task_2.py
+++ lti_server/server/infra/tasks/task_2.py
@@ -3,8 +3,8 @@ import os
 from .ripes_parser import parse_ripes
 
 def check(student_id):
-	run = parse_ripes(subprocess.check_output("{d}/Ripes --mode cli --src {d}/tmp_solution/{sid}_2.s -t asm --proc RV64_SS --regs --json -v".format(d=os.path.dirname(__file__), sid = student_id).split()).decode("utf-8"))	
-	return "All tests passed!"
+	run = parse_ripes(subprocess.check_output("{d}/Ripes --appimage-extract-and-run --mode cli --src {d}/tmp_solution/{sid}_2.s -t asm --proc RV64_SS --regs --json -v".format(d=os.path.dirname(__file__), sid = student_id).split()).decode("utf-8"))	
+	return "All tests for the second task passed!"
 	
 def get_txt():
 	return ["task2_title", "task2_text", 2]	
