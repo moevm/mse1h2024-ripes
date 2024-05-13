0d8026cb92dbf81785c76effa3d67c9da0fdf23e
mantsevat <mantsevat@gmail.com>
2024-05-13 17:05:27 +0300

    add simple check for two tasks

--- lti_server/server/infra/tasks/check/check_2.py
+++ lti_server/server/infra/tasks/check/check_2.py
@@ -7,14 +7,16 @@ def check(student_id, condition):
 	params[1]=int(params[1][7:])
 	ev1 = answer(params)
 	run1 = parse_ripes(subprocess.check_output("{d}/Ripes --appimage-extract-and-run --mode cli --src {d}/tmp_solution/{sid}_2.s -t asm --proc RV64_SS --regs --json -v".format(d=os.path.dirname(__file__), sid = student_id).split()).decode("utf-8"))	
-	return "All tests for the second task passed!"
+	return "Всё верно" if (ev1 == run1[1].get("registers").get("x6") ) else "Проверка не пройдена"
 	
 def answer(params):
 	b = bin(params[1])[2:]
 	zeroes = b.count('0')
 	ones = len(b) - zeroes
 	
-	if (zeroes >= ones):
+	if zeroes == 0 : return 0 
+	
+	if zeroes >= ones:
 		greater = zeroes
 		smaller = ones
 	else:
--- lti_server/server/infra/tasks/check/check_3.py
+++ lti_server/server/infra/tasks/check/check_3.py
@@ -4,5 +4,5 @@ from .ripes_parser import parse_ripes
 
 def check(student_id, condition):
 	run1 = parse_ripes(subprocess.check_output("{d}/Ripes --appimage-extract-and-run --mode cli --src {d}/tmp_solution/{sid}_3.s -t asm --proc RV64_SS --regs --json -v".format(d=os.path.dirname(__file__), sid = student_id).split()).decode("utf-8"))	
-	return "All tests for the third task passed!"
+	return "Для этого задания пока нет проверки"
 	
