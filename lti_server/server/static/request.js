b69e30b44da76a77a8685ba002af87992b501e42
Манцева Татьяна <55297427+mantsevat@users.noreply.github.com>
2024-05-13 14:49:49 +0300

    fix argument absence in request.js

--- lti_server/server/static/request.js
+++ lti_server/server/static/request.js
@@ -4,7 +4,7 @@ async function get_task_list()
 	return response.json();
 }
 
-async function send_solution(str_solution, task_id)
+async function send_solution(str_solution, str_condition, task_id)
 {
 	let response = await fetch("http://127.0.0.1:5000/check", {method: "POST", headers: {"Accept": "application/json", "Content-Type": "application/json"}, body: JSON.stringify({user: uid, task: task_id, solution: str_solution, condition: str_condition })});
 	return response.json();
